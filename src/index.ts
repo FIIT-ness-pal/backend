429d1722d46306cccc7f28417a0c0c226790a7da
Jakub <jakub.abrahoim@windowslive.com>
2022-03-10 19:28:02 +0100

    express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
+import * as express from "express";
 
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+/*
 createConnection().then(async connection => {
 
     console.log("Ahoj");
 
-}).catch(error => console.log(error));
+}).catch(error => console.log(error));*/
