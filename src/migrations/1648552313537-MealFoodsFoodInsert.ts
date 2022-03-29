47d04bfda0c2a3e5eb01af2e6ccfc57db801b32b
Jakub <jakub.abrahoim@windowslive.com>
2022-03-29 13:23:49 +0200

    MealFoodsFoodInsert migration

---
+++ src/migrations/1648552313537-MealFoodsFoodInsert.ts
@@ -0,0 +1,21 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class MealFoodsFoodInsert1648552313537 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            INSERT INTO public.meal_foods_food ("mealId", "foodId")
+                VALUES ('38e011fc-375f-4f7b-84f8-e085935ec9c9', '7a39795d-dc1b-4857-9b41-84e1ac9029c2');
+            INSERT INTO public.meal_foods_food ("mealId", "foodId")
+                VALUES ('38e011fc-375f-4f7b-84f8-e085935ec9c9', 'f364274d-7429-4e8d-ae03-574e90f4b147');
+            INSERT INTO public.meal_foods_food ("mealId", "foodId")
+                VALUES ('b601b5ee-4bd0-4db2-83b4-7d27d6e1d701', 'c580405c-d8da-4fe4-b1d2-cf7e2134ef43');
+            INSERT INTO public.meal_foods_food ("mealId", "foodId")
+                VALUES ('b601b5ee-4bd0-4db2-83b4-7d27d6e1d701', 'b7d139d0-50df-46fa-90a4-a0f0440e401a');
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
